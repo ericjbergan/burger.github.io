8598f449f8235ac153464d9e4724dd713f50f971
Eric Bergan <bootcamp.ejb@gmail.com>
2019-09-17 08:20:13 -0500

    still trying to fix heroku

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgerdb"
-});
-
-// var connection = mysql.createConnection({
-//   host: "y5s2h87f6ur56vae.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   port: 3306,
-//   user: "xrraaz1o17wis418",
-//   password: "mh6u7iabm9req3sr",
-//   database: "r5otf2xm12zxtv7w"
-// });
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burger_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) { 
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -28,4 +24,4 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-module.exports = connection; 
+module.exports = connection;
